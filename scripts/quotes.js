dff9d876be2156d504bc5cad9db3933473285a30
nadiatulhamdan <152396636+nadiatulhamdan@users.noreply.github.com>
2025-05-20 16:48:14 +0800

    Update quotes.js

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,5 +1,5 @@
 async function getQuote() {
-  const res = await fetch('https://api.quotable.io/random');
+  const res = await fetch('https://zenquotes.io/api/random');
   const data = await res.json();
 
   document.getElementById('quoteText').innerText = `"${data.content}"`;
